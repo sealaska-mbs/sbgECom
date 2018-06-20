adfc99ba0f9c98a662be5057ef06e35b485057a4
s <s>
2018-06-20 11:18:28 +0800

    fix SBG_TIME_OUT error, change com baud rate to 921600 learning from sbgCenter

--- Examples/ellipseMinimal/src/ellipseMinimal.c
+++ Examples/ellipseMinimal/src/ellipseMinimal.c
@@ -81,7 +81,7 @@ int main(int argc, char** argv)
 	// Note interface closing is also differentiated !
 	//
 	//errorCode = sbgInterfaceSerialCreate(&sbgInterface, "/dev/cu.usbserial-FTX2GE57", 921600);		// Example for Unix using a FTDI Usb2Uart converter
-	errorCode = sbgInterfaceSerialCreate(&sbgInterface, "COM3", 115200);							// Example for Windows serial communication
+	errorCode = sbgInterfaceSerialCreate(&sbgInterface, "COM3", 921600);							// Example for Windows serial communication
 
 	//
 	// Test that the interface has been created
